b0fd6dc55f4af24284430a508e28088580fc5295
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-06-08 15:07:11 -0700

    setup project api fetched

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import { fetchWeather } from './api/fetchWeather'
+import './App.css';
+
+
+const App = () => {
+    const [query, setQuery] = useState('');
+
+    const search = async (e) => {
+        if (e.key === 'Enter') {
+            const data = await fetchWeather(query)
+
+            console.log(data)
+
+        }
+    }
+    return (
+        <div classNme="main-container">
+            <input
+                type="text"
+                className="search"
+                placeholder="Search ..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+            />
+
+
+
+        </div>
+    );
+}
+
+export default App;
